f0569cb42b757705ebd1f70f55bd2736509746fa
Andrew <99478439+AndrewPerevoshchenko@users.noreply.github.com>
2023-05-08 16:41:20 +0300

    Added comments for TrafficLight.cs

--- Home_task_7/TrafficLight.cs
+++ Home_task_7/TrafficLight.cs
@@ -9,11 +9,11 @@ namespace Home_task_7
     internal enum TrafficColour: byte
     {
         Red,
-        YellowBR,
-        YellowBG,
+        YellowBR, //Жовтий перед червоним
+        YellowBG, //Жовтий перед зеленим
         Green
     }
-    internal enum GeographicalDirection: sbyte
+    internal enum GeographicalDirection: sbyte //Напрямки (протилежні мають протилежні за знаком константи)
     {
         North = 1,
         South = -1,
@@ -25,11 +25,11 @@ namespace Home_task_7
         SouthWest = -4
     }
     public delegate string ColourWork();
-    internal class TrafficLight: IComparable<TrafficLight>
+    internal class TrafficLight: IComparable<TrafficLight>  //Була ідея із сортуванням. У майбутньому може згодитися задля зручного групування (коли напрямки будуть не протилежні, то не треба буде важко шукати й групувати)
     {
-        public static int DIRECTION_MAX_LENGTH_STR { get; private set; }
-        private (GeographicalDirection, GeographicalDirection) _direction;
-        private TrafficColour _colour;
+        public static int DIRECTION_MAX_LENGTH_STR { get; private set; } //Розмір для форматування по "Напрямку"
+        private (GeographicalDirection, GeographicalDirection) _direction; //Напрямки "від" та "до"
+        private TrafficColour _colour; //Колір світлофора
         public (GeographicalDirection, GeographicalDirection) Direction
         {
             get { return _direction; }
@@ -39,15 +39,15 @@ namespace Home_task_7
             get { return _colour; }
             set { _colour = value; }
         }
-        public event ColourWork? ColourChanged;
+        public event ColourWork? ColourChanged; //Подія, що відповідає за зміну кольору
         public TrafficLight(in GeographicalDirection directionFrom, in GeographicalDirection directionTo, in TrafficColour trafficColour = TrafficColour.Red)
         {
-            if ((sbyte)directionFrom == -(sbyte)directionTo)
+            if ((sbyte)directionFrom == -(sbyte)directionTo) //Якщо напрямки протилежні (для даної задачі)
             {
                 _direction = (directionFrom, directionTo);
                 _colour = trafficColour;
             }
-            else
+            else //Інакше, робимо протилежними відносно "від"
             {
                 _direction = (directionFrom, (GeographicalDirection)(-(sbyte)directionFrom));
                 _colour = trafficColour;
@@ -61,7 +61,7 @@ namespace Home_task_7
                 DIRECTION_MAX_LENGTH_STR = directionTo.ToString().Length;
             }           
         }
-        public double ColourNext()
+        public double ColourNext() //Заміна кольору на наступний з виводом майбутнього тайму
         {
             double result = -1;
             switch (_colour)
@@ -86,7 +86,7 @@ namespace Home_task_7
                     result = 0;
                     break;
             }
-            ColourChanged?.Invoke();
+            ColourChanged?.Invoke(); //А тут уже й сама подія
             return result;
         }
         public int CompareTo(TrafficLight? other)
